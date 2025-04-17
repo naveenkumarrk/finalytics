db83bbc314c64e5b4e03fee759649d38a784ecf2
naveenrk7 <111naveenkumarrk@gmail.com>
2025-04-18 03:21:07 +0530

    cors error

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173'], // Your Vite client URL
+  origin: ['http://localhost:5173', 'https://finalytics-lac.vercel.app', 'https://finalytics-gtve.vercel.app'], // Your Vite client URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
